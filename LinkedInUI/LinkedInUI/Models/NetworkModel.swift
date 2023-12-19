e809f407b0d4dfdd40d66f9cd26609f6afead933
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2023-12-20 00:11:19 +0530

    Added Models and updated README file.

---
+++ LinkedInUI/LinkedInUI/Models/NetworkModel.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkModel.swift
+//  LinkedInUI
+//
+//  Created by Laxman Singh Koranga on 20/12/2023.
+//
+
+import Foundation
+
+struct NetworkModel {
+    var id: Int
+    var name: String
+    var position: String
+    var mutual: String
+    var image: String
+}
---
+++ LinkedInUI/LinkedInUI/Models/PostModel.swift
@@ -0,0 +1,16 @@
+//
+//  PostModel.swift
+//  LinkedInUI
+//
+//  Created by Laxman Singh Koranga on 20/12/2023.
+//
+
+import Foundation
+
+struct PostData {
+    var id: Int
+    var Image: String
+    var title: String
+    var followers: Int
+    var profileImage: String
+}
