914c582d33f7d0589a2f22982c70118aedfe4b2c
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2023-12-20 19:07:13 +0530

    Added user feed home card.

---
+++ LinkedInUI/LinkedInUI/Views/PostCardView.swift
@@ -0,0 +1,65 @@
+//
+//  PostCardView.swift
+//  LinkedInUI
+//
+//  Created by Laxman Singh Koranga on 20/12/2023.
+//
+
+import SwiftUI
+
+struct SocialView {
+    var ids: Int
+    var image: String
+    var title: String
+}
+
+var socialView: [SocialView] = [
+    .init(ids: 0, image: "hand.thumbsup", title: "Like"),
+    .init(ids: 1, image: "text.bubble", title: "Like"),
+    .init(ids: 2, image: "arrow.turn.up.right", title: "Share"),
+    .init(ids: 3, image: "paperplane.fill", title: "Send"),
+]
+
+let samplePostData = PostData(id: 1, Image: "02", title: "Laxman Singh Koranga", followers: 33, profileImage: "1")
+
+struct PostCardView: View {
+    var data: PostData
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Rectangle()
+                .fill(.gray.opacity(0.3))
+                .frame(width: .infinity, height: 10)
+                .ignoresSafeArea(.all)
+            HStack(alignment: .center) {
+                Image(data.profileImage)
+                    .resizable()
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 70, height: 70, alignment: .leading)
+                VStack(alignment: .leading) {
+                    Text(data.title)
+                        .font(.body)
+                    Text("\(data.followers) followers")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                    Text("9m")
+                        .font(.caption)
+                        .foregroundColor(.gray)
+                }
+                Spacer()
+                Image(systemName: "ellipsis")
+            }.padding(.horizontal)
+            Text("Looking for a new course on iOS with SwiftUI, you are already at great place.")
+            Image(data.Image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: .infinity, height: .infinity, alignment: .center)
+            Divider()
+        }
+    }
+}
+
+#Preview {
+    PostCardView(data: samplePostData)
+}
