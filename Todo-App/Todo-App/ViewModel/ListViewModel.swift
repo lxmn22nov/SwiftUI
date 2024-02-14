554eb7aeb254457f4d8924cf965a64201b7a288b
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2024-02-14 18:02:02 +0530

    Updated ListViewModel.

--- Todo-App/Todo-App/ViewModel/ListViewModel.swift
+++ Todo-App/Todo-App/ViewModel/ListViewModel.swift
@@ -9,9 +9,29 @@ import Foundation
 
 class ListViewModel: ObservableObject {
     
-    @Published var items: [ItemModel] = [ ]
+    @Published var items: [ItemModel] = [ ] {
+        didSet {
+            saveItems()
+        }
+    }
     
     let itemsKey: String = "items_List"
+    
+    init() {
+        getItems()
+    }
+    
+    // Retreving or getting the empty/full array.
+    func getItems() {
+        // storing user JSON data.
+        guard let data = UserDefaults.standard.data(forKey: itemsKey) else { return }
+        
+        // saving user JSON data in our specified data format.
+        guard let savedItems = try? JSONDecoder().decode([ItemModel].self, from: data) else { return }
+        
+        // saving data into items variable.
+        self.items = savedItems
+    }
     
     func saveItems() {
         if let encodeData = try? JSONEncoder().encode(items) {
