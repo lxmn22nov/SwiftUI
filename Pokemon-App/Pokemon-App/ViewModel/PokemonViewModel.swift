d2cf6ab05b2036c072e2f03b16281d86cefaed9f
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2024-02-21 23:26:38 +0530

    Added updated files.

--- Pokemon-App/Pokemon-App/ViewModel/PokemonViewModel.swift
+++ Pokemon-App/Pokemon-App/ViewModel/PokemonViewModel.swift
@@ -12,11 +12,18 @@ class PokemonViewModel: ObservableObject {
     
     let baseURL = "https://pokedex-bb36f.firebaseio.com/pokemon.json"
     
+    init() {
+        fetchData()
+    }
+    
     func fetchData() {
         guard let url = URL(string: baseURL) else { return }
         
         URLSession.shared.dataTask(with: url) {
             (data, response, error) in
+            
+            
+            guard let data = data?.parseData(removeString: "null,") else { return }
             
             guard let pokemon = try? JSONDecoder().decode([Pokemon].self, from: data) else { return }
             
@@ -25,5 +32,31 @@ class PokemonViewModel: ObservableObject {
                 self.pokemon = pokemon
             }
         }.resume()
+    }
+    
+    func backgroundColor(forType type: String) -> UIColor {
+        switch type {
+        case "fire": return .orange
+        case "poison": return .purple
+        case "water": return .blue
+        case "electric": return .yellow
+        case "psychic": return .brown
+        case "normal": return .magenta
+        case "ground": return .green
+        case "flying": return .cyan
+        case "fairy": return .systemTeal
+        default: return .systemPink
+        }
+    }
+}
+
+extension Data {
+    func parseData(removeString string: String) -> Data? {
+        let dataAsString = String(data: self, encoding: .utf8)
+        
+        let parsedDataString = dataAsString?.replacingOccurrences(of: string, with: "")
+        
+        guard let data = parsedDataString?.data(using: .utf8) else { return nil }
+        return data
     }
 }
