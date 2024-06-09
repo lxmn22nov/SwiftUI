700d9e03259fd38bd971dc6aa4018d72e230d27a
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2024-06-09 16:59:20 +0530

    Added View to Core & updated the AirBNB files.

---
+++ AirBNB Clone/AirBNB Clone/Core/Explore/DestinationSearchView.swift
@@ -0,0 +1,136 @@
+//
+//  DestinationSearchView.swift
+//  AirBNB Clone
+//
+//  Created by Laxman Singh Koranga on 09/06/2024.
+//
+
+import SwiftUI
+
+enum DestinationSearchOptions {
+    case location
+    case dates
+    case guests
+}
+
+struct DestinationSearchView: View {
+    @Binding var show: Bool
+    @State private var destination = ""
+    @State private var selectedOption: DestinationSearchOptions = .location
+    
+    var body: some View {
+        VStack {
+            // cross button which redirects to the explore view.
+            Button {
+                withAnimation(.snappy) {
+                    show.toggle()
+                }
+            } label: {
+                Image(systemName: "xmark.circle")
+                    .imageScale(.large)
+                    .foregroundStyle(.black)
+            }
+            // Search Bar.
+            VStack(alignment: .leading) {
+                if selectedOption == .location {
+                    Text("Where to?")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .imageScale(.small)
+                        TextField("Search destinations", text: $destination)
+                            .font(.subheadline)
+                    }
+                    .frame(height: 44)
+                    .padding(.horizontal)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 9)
+                            .stroke(lineWidth: /*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/)
+                            .foregroundStyle(Color(.systemGray4))
+                    }
+                } else {
+                    CollapsedPickerView(title: "Where", description: "Add Destination")
+                }
+            }
+            .padding()
+            .frame(height: selectedOption == .location ? 120 : 64)
+            .background(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            .padding()
+            .shadow(radius: 10)
+            .onTapGesture {
+                withAnimation(.snappy) { selectedOption = .location }
+            }
+            
+            // Date Selection View.
+            VStack {
+                if selectedOption == .dates {
+                    HStack {
+                        Text("Show expanded view.")
+                        Spacer()
+                    }
+                } else {
+                    CollapsedPickerView(title: "When", description: "Add Dates")
+                }
+            }
+            .padding()
+            .frame(height: selectedOption == .dates ? 120 : 64)
+            .background(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            .padding()
+            .shadow(radius: 10)
+            .onTapGesture {
+                withAnimation(.snappy) { selectedOption = .dates }
+            }
+
+            // Number of guests View.
+            VStack {
+                if selectedOption == .guests {
+                    HStack {
+                        Text("Show expanded view.")
+                        Spacer()
+                    }
+                } else {
+                    CollapsedPickerView(title: "Who", description: "Add Guests")
+                }
+            }
+            .padding()
+            .frame(height: selectedOption == .guests ? 120 : 64)
+            .background(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            .padding()
+            .shadow(radius: 10)
+            .onTapGesture {
+                withAnimation(.snappy) { selectedOption = .guests }
+            }
+        }
+    }
+}
+
+#Preview {
+    DestinationSearchView(show: .constant(false))
+}
+
+struct CollapsedPickerView: View {
+    let title: String
+    let description: String
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text(title)
+                    .foregroundStyle(.gray)
+                Spacer()
+                Text(description)
+            }
+            .fontWeight(.semibold)
+            .font(.subheadline)
+        }
+//        .padding()
+//        .background(.white)
+//        .clipShape(RoundedRectangle(cornerRadius: 12))
+//        .padding()
+//        .shadow(radius: 10)
+    }
+}
--- AirBNB Clone/AirBNB Clone/Core/Explore/ExploreView.swift
+++ AirBNB Clone/AirBNB Clone/Core/Explore/ExploreView.swift
@@ -8,29 +8,40 @@
 import SwiftUI
 
 struct ExploreView: View {
+    @State private var showDestinationSearchView = false
+    
     var body: some View {
         // NavigationStack used to jumps to next screen.
         NavigationStack {
-            // ScrollView helps the rectangle frames to scroll over the screen.
-            ScrollView {
-                SearchAndFilterBar()
-                // LazyStack helps items to grow according to the need.
-                LazyVStack(spacing: 33) {
-                    ForEach(0...4, id: \.self) { listing in
-//                        Rectangle()
-                        // NavigationLink will help user to navigate through ExploreView to Detail of ListingItemView.
-                        NavigationLink(value: listing) {
-                            ListingItemView()
-                                .frame(height: 400)
-                                .clipShape(RoundedRectangle(cornerRadius: 9))
+            if showDestinationSearchView {
+                DestinationSearchView(show: $showDestinationSearchView)
+            } else {
+                // ScrollView helps the rectangle frames to scroll over the screen.
+                ScrollView {
+                    SearchAndFilterBar()
+                        .onTapGesture {
+                            withAnimation(.snappy) {
+                                showDestinationSearchView.toggle()
+                            }
+                        }
+                    // LazyStack helps items to grow according to the need.
+                    LazyVStack(spacing: 33) {
+                        ForEach(0...4, id: \.self) { listing in
+    //                        Rectangle()
+                            // NavigationLink will help user to navigate through ExploreView to Detail of ListingItemView.
+                            NavigationLink(value: listing) {
+                                ListingItemView()
+                                    .frame(height: 400)
+                                    .clipShape(RoundedRectangle(cornerRadius: 9))
+                            }
                         }
                     }
+                    .padding()
                 }
-                .padding()
-            }
-            .navigationDestination(for: Int.self) { listing in
-                ListingDetailView()
-                    .navigationBarBackButtonHidden()
+                .navigationDestination(for: Int.self) { listing in
+                    ListingDetailView()
+                        .navigationBarBackButtonHidden()
+                }
             }
         }
     }
