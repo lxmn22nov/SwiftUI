e78161caaf18d1767530c49a38492c5b6f0dc31c
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2024-07-03 22:39:27 +0530

    Added new folder AppFeatures consists apps features.

---
+++ AppFeatures/DropDownPicker/DropDownPicker/App/DropDownPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropDownPickerApp.swift
+//  DropDownPicker
+//
+//  Created by Laxman Singh Koranga on 03/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct DropDownPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
