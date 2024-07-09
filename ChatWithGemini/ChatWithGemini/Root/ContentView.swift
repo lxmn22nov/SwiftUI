66955b38a1a1b78cb119d826dc5be15950515762
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2024-07-09 14:57:07 +0530

    Added ChatWithGemini Project.

---
+++ ChatWithGemini/ChatWithGemini/Root/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChatWithGemini
+//
+//  Created by Laxman Singh Koranga on 09/07/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
