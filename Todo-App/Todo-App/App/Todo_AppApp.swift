3ffaaa57e4f970f8f891816070a4992b082e5fae
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2024-07-15 12:23:11 +0530

    Added App folder.

---
+++ Todo-App/Todo-App/App/Todo_AppApp.swift
@@ -0,0 +1,23 @@
+//
+//  Todo_AppApp.swift
+//  Todo-App
+//
+//  Created by Laxman Singh Koranga on 14/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Todo_AppApp: App {
+    // State Management.
+    @StateObject var listViewModel: ListViewModel = ListViewModel()
+    
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ListView()
+            }
+            .environmentObject(listViewModel)
+        }
+    }
+}
