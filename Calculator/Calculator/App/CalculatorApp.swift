4f2c7cf4f67c9bed32a8a789095cfeca314d3920
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2024-07-22 03:14:01 +0530

    Added updated files.

---
+++ Calculator/Calculator/App/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Laxman Singh Koranga on 16/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
