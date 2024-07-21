5233530b4e2fe5e659785be1190a045de14c02a8
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2024-07-22 03:15:25 +0530

    Added image constants to the project.

---
+++ Calculator/Calculator/Model/ImageConstants.swift
@@ -0,0 +1,18 @@
+//
+//  ImageConstants.swift
+//  Calculator
+//
+//  Created by Laxman Singh Koranga on 21/07/2024.
+//
+
+import Foundation
+import SwiftUI
+
+let buttonBackgroundColor = Color("buttonBackground")
+let foregroundDigitsColor = Color("foregroundDigits")
+let foregroundRightButtonsColor = Color("foregroundRightButtons")
+let foregroundTopButtonsColor = Color("foregroundTopButtons")
+let primaryBackgroundColor = Color("primaryBackground")
+let secondaryBackgroundColor = Color("secondaryBackground")
+let sunOrMoonNotSelectedColor = Color("sunOrMoonSelected")
+let sunOrMoonSelectedColor = Color("sunOrMoonSelected")
